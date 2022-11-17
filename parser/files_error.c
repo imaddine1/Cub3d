2b5f61941b9812ccea20f37da3fa9366c22d1238
imaddine1 <hh.imad2015@gmail.com>
2022-11-17 11:31:26 +0100

    fix cheking on extention

--- parser/files_error.c
+++ parser/files_error.c
@@ -6,7 +6,7 @@
 /*   By: iharile <iharile@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 13:36:00 by iharile           #+#    #+#             */
-/*   Updated: 2022/11/10 11:05:25 by iharile          ###   ########.fr       */
+/*   Updated: 2022/11/17 11:30:15 by iharile          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	check_extension(char *str)
 
 	i = 0;
 	new_str = ft_strrchr(str, '.');
-	if (ft_strncmp(new_str, ".cub", ft_strlen(new_str)))
+	if (ft_strncmp(new_str, ".cub\0", 5))
 	{
 		msg_exit("the extension of the file must be .cub");
 	}
